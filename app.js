caaea108a07fae4cf0a6e15d65ab48c883904473
ClaudHu <claud945@gmail.com>
2016-02-25 22:18:06 +0800

    更新

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var apps = require('./routes/apps');
 var app = express();
 
 // view engine setup
@@ -24,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/apps', apps);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/apps.js
+++ routes/apps.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/', function(req, res, next) {
+router.get('/drshine/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
