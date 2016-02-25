e475d577920e7e585478a5e9a890c7b42721d5db
ClaudHu <claud945@gmail.com>
2016-02-25 22:31:41 +0800

    v0.0000001

--- routes/apps.js
+++ routes/apps.js
@@ -6,8 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/drshine/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/drshine/', function(req, res, next) {  //光澤診所的路由
+  res.render('drshine', { title: '光澤心理遊戲' });
 });
 
 module.exports = router;
